661f6f88fd523af198998941bbba6048e63952a1
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2023-04-17 12:47:22 +0530

    remove console

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,6 +5,5 @@ import { sequence } from "@sveltejs/kit/hooks";
 const deviceDetector = handleDeviecDetector({})
 
 export const handle = sequence(deviceDetector, ({ resolve, event }) => {
-    console.log(event.locals.deviceType)
 	return resolve(event);
 });
